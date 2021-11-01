d7868ff717f6b5b2d38aacb73d66acfeb969fbc7
shaheerashan <77646573+shaheerashan@users.noreply.github.com>
2021-10-31 20:43:20 -0400

    Create read file systems

--- server.js
+++ server.js
@@ -1,17 +1,10 @@
-const http = require('http');
+const { readFile, readFileSync } = require('fs')
 
-const HOSTNAME = process.env.HOSTNAME || 'localhost';
-const PORT = process.env.PORT || 4000;
+try {
+const data = readFileSync("hi.txt", 'utf8');
+console.log(data);
+} catch(err) {
+  console.error(err);
+}
 
-const server = http.createServer((request, response) => {
-    response.statusCode = 200;
-    response.setHeader('Content-Type', 'text/plain');
-    response.end('Hello World');
-  });
-
-  server.listen(PORT, HOSTNAME, () => {
-    console.log(`Server is running at http://${HOSTNAME}:${PORT}/`);
-  });
-  
-  console.log(__filename);
-  console.log(__dirname);
+  console.log("Log from outside");
