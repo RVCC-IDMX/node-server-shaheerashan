03cb95ffb819fa22f1b53cb0c189f7aa80166895
shaheerashan <77646573+shaheerashan@users.noreply.github.com>
2021-10-31 20:31:53 -0400

    Create hello world example

---
+++ server.js
@@ -0,0 +1,17 @@
+const http = require('http');
+
+const HOSTNAME = process.env.HOSTNAME || 'localhost';
+const PORT = process.env.PORT || 4000;
+
+const server = http.createServer((request, response) => {
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'text/plain');
+    response.end('Hello World');
+  });
+
+  server.listen(PORT, HOSTNAME, () => {
+    console.log(`Server is running at http://${HOSTNAME}:${PORT}/`);
+  });
+  
+  console.log(__filename);
+  console.log(__dirname);
