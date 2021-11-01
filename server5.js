ede3bd7f8b25546323059beed4f84a3e3f786960
shaheerashan <77646573+shaheerashan@users.noreply.github.com>
2021-10-31 21:35:43 -0400

    Update page 404

--- server5.js
+++ server5.js
@@ -18,7 +18,7 @@ const server = createServer((req, res) => {
             break;
         default:
             path += '404.html';
-            res.statusCode = 200;
+            res.statusCode = 404;
             break;
     }
 
