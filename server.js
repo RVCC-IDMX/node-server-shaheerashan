d4e86499a2ef987a67cb1b7ce207be30e502ea4f
shaheerashan <77646573+shaheerashan@users.noreply.github.com>
2021-11-02 15:20:50 -0400

    Fix errors

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
-const http = require('http');
+import { createServer } from 'http';
 
 const HOSTNAME = process.env.HOSTNAME || 'localhost';
 const PORT = process.env.PORT || 3000;
+const server = createServer((request, response) => {
+  response.statusCode = 200;
+  response.setHeader('Content-Type', 'text-plain');
+  response.end('Hello World');
+});
 
-window.document;
+server.listen(PORT, HOSTNAME, () => {
+  console.log(`Server running at http://${HOSTNAME}:${PORT}/`);
+});
--- server5.js
+++ server5.js
@@ -1,36 +1,36 @@
-const http = require("http");
-const fs = require("fs");
+const http = require('http');
+const fs = require('fs');
 
 const PORT = process.env.PORT || 3000;
 
-const server = createServer((req, res) => {
-    res.setHeader('Content-Type', 'text/html');
+const server = http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'text/html');
 
-    let path = './';
-    switch(req.url) {
-        case '/':
-            path += 'index.html';
-            res.statusCode = 200;
-            break;
-        case '/about':
-            path += 'about.html';
-            res.statusCode = 200;
-            break;
-        default:
-            res.setHeader('Location', '/')
-            res.statusCode = 301;
-            break;
+  let path = './';
+  switch (req.url) {
+    case '/':
+      path += 'index.html';
+      res.statusCode = 200;
+      break;
+    case '/about':
+      path += 'about.html';
+      res.statusCode = 200;
+      break;
+    default:
+      path += '404.html';
+      res.statusCode = 404;
+      break;
+  }
+
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      console.error(err);
+      res.end();
+    } else {
+      res.write(data);
+      res.end();
     }
-
-    readFile(path, (err, data) => {
-        if(err) {
-            console.error(err);
-            res.end()
-        } else {
-            res.write(data);
-            res.end();
-        }
-    });
-})
+  });
+});
 
 server.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
